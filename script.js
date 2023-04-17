ad99af2e3603ec171efbc504e0740a5cf7a9fa5b
Saviour Ise <65181992+Saviourise@users.noreply.github.com>
2023-04-17 15:04:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const leftScroll = () => {
+  const left = document.querySelector(".scroll-images");
+  left.scrollBy(-450, 0);
+};
+function rightScroll() {
+  const right = document.querySelector(".scroll-images");
+  right.scrollBy(450, 0);
+}
+
+document.querySelector(".hr").getElementsByClassName.width = window.screen.innerWidth;
